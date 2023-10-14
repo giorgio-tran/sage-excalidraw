e69d670367617caec2ed8de623eb21dfb7d53b6c
Giorgio <tran.giorgio@gmail.com>
2023-10-13 23:13:08 -1000

    Get canvas to fill up viewport and test drawing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = (canvas as HTMLCanvasElement).getContext('2d');
 
+    (ctx as CanvasRenderingContext2D).fillStyle = "green";
+    (ctx as CanvasRenderingContext2D).fillRect(10, 10, 150, 100);
+    (ctx as CanvasRenderingContext2D).strokeRect(200, 200, 100, 100);
+
+  }, [])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight}/>
   )
 }
 
