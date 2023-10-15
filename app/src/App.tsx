33d56b2b773f63a120a29801b4c005658c53cc9a
Giorgio <tran.giorgio@gmail.com>
2023-10-14 23:52:38 -1000

    Add ability to move shapes drawn

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,39 +4,84 @@ import { Drawable } from "roughjs/bin/core";
 
 const generator = rough.generator();
 
+type ToolType = "line" | "rectangle" | "selection";
 type ElementType = {
+  id: number;
   x1: number;
   y1: number;
   x2: number;
   y2: number;
-  roughElement: Drawable;
-  shape: "line" | "rectangle";
+  roughElement?: Drawable;
+  offset?: { x: number; y: number };
+  shape: ToolType;
 };
 
 function createElement(
+  id: number,
   x1: number,
   y1: number,
   x2: number,
   y2: number,
-  shape: "line" | "rectangle"
+  shape: ToolType
 ) {
   if (shape === "line") {
     const roughElement = generator.line(x1, y1, x2, y2);
-    return { x1, y1, x2, y2, roughElement, shape };
+    return { id, x1, y1, x2, y2, roughElement, shape };
   }
   if (shape === "rectangle") {
     const roughElement = generator.rectangle(x1, y1, x2 - x1, y2 - y1);
-    return { x1, y1, x2, y2, roughElement, shape };
+    return { id, x1, y1, x2, y2, roughElement, shape };
   }
+}
+
+const getElementAtPosition = (x: number, y: number, elements: ElementType[]) => {
+  return elements.find(element => isWithinElement(x, y, element));
+
+}
+
+/**
+ * Gets the existing mouse position, and checks if it is within the drawn element
+ * @param x 
+ * @param y 
+ * @param element 
+ * @returns 
+ */
+const isWithinElement = (x: number, y: number, element: ElementType) => {
+  const { x1, x2, y1, y2, shape } = element;
+  if (shape === "rectangle") {
+    const minX = Math.min(x1, x2);
+    const maxX = Math.max(x1, x2);
+    const minY = Math.min(y1, y2);
+    const maxY = Math.max(y1, y2);
+    return x >= minX && x < maxX && y >= minY && y < maxY;
+  }
+  if (shape === "line") {
+    const a = { x: x1, y: y1 };
+    const b = { x: x2, y: y2 };
+    const c = { x, y };
+    const offset = distance(a, b) - (distance(a, c) + distance(b, c));
+    return Math.abs(offset) < 1;
+  }
+}
+
+/**
+ * Distance formula
+ * @param a 
+ * @param b 
+ * @returns 
+ */
+const distance = (a: { x: number; y: number }, b: { x: number; y: number }) => { 
+  const dx = a.x - b.x;
+  const dy = a.y - b.y;
+  return Math.sqrt(dx * dx + dy * dy);
 }
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [drawing, setDrawing] = useState(false);
+  const [action, setAction] = useState("none");
   const [elements, setElements] = useState<ElementType[]>([]);
-  const [elementType, setElementType] = useState<"line" | "rectangle" | null>(
-    null
-  );
+  const [tool, setToolType] = useState<ToolType | null>(null);
+  const [selectedElement, setSelectedElement] = useState<ElementType | null>(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -45,43 +90,97 @@ function App() {
 
     const roughCanvas = rough.canvas(canvas as HTMLCanvasElement);
 
-    elements.forEach(({ roughElement }) => roughCanvas.draw(roughElement));
+    elements.forEach(({ roughElement }) =>
+      roughCanvas.draw(roughElement as Drawable)
+    );
   }, [elements]);
 
-  const handleMouseDown = (event: React.MouseEvent<HTMLCanvasElement>) => {
-    if (!elementType) return;
-    setDrawing(true);
-    const { clientX, clientY } = event;
-    const element = createElement(
-      clientX,
-      clientY,
-      clientX,
-      clientY,
-      elementType as "line" | "rectangle"
+  const updateElement = ({id, x1, y1, x2, y2, shape}: ElementType) => {
+    console.log(id, x1, y1, x2, y2, shape);
+    const updatedElement = createElement(
+      id,
+      x1,
+      y1,
+      x2,
+      y2,
+      shape as ToolType
     );
-    setElements((prevState) => [...prevState, element] as ElementType[]);
+    const elementsCopy = [...elements];
+    elementsCopy[id] = updatedElement as ElementType;
+    setElements(elementsCopy);
+    console.log(x2, y2);
+  }
+  // MOUSE EVENTS
+  const handleMouseDown = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    if (!tool) return;
+    const { clientX, clientY } = event;
+
+    if (tool === "selection") {
+      const element = getElementAtPosition(clientX, clientY, elements);
+      if (element) {
+        // calculate the offset to that drawn elements 0,0 position doesn't jump to mouse
+        const offsetX = clientX - element.x1;
+        const offsetY = clientY - element.y1;
+        setSelectedElement({...element, offset: { x: offsetX, y: offsetY }});
+        setAction("moving");
+      }
+
+    } else {
+      const id = elements.length;
+      const element = createElement(
+        id,
+        clientX,
+        clientY,
+        clientX,
+        clientY,
+        tool as ToolType
+      );
+      setElements((prevState) => [...prevState, element] as ElementType[]);
+      setAction("drawing");
+    }
   };
 
   const handleMouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
-    if (!drawing) return;
     const { clientX, clientY } = event;
-    const index = elements.length - 1;
-    const { x1, y1 } = elements[index];
+    if (action === "none") return;
 
-    const updatedElement = createElement(
-      x1,
-      y1,
-      clientX,
-      clientY,
-      elementType as "line" | "rectangle"
-    );
-    const elementsCopy = [...elements];
-    elementsCopy[index] = updatedElement as ElementType;
-    setElements(elementsCopy);
-    console.log(clientX, clientY);
+    if (action === "drawing") {
+      // same as id since we only draw one at a time
+      const index = elements.length - 1;
+      const { x1, y1 } = elements[index];
+
+      updateElement({
+        id: index,
+        x1: x1,
+        y1: y1,
+        x2: clientX,
+        y2: clientY,
+        shape: tool as ToolType
+      });
+    }
+
+    if (action === "moving") {
+      const {id, x1, y1, x2, y2, shape, offset} = selectedElement as ElementType;
+      const width = x2 - x1;
+      const height = y2 - y1;
+      const offsetX = offset?.x ?? 0;
+      const offsetY = offset?.y ?? 0;
+      const mousePositionX = clientX - offsetX;
+      const mousePositionY = clientY - offsetY;
+      updateElement({
+        id: id,
+        x1: mousePositionX,
+        y1: mousePositionY,
+        x2: mousePositionX + width,
+        y2: mousePositionY + height,
+        shape: shape as ToolType
+      });
+    }
   };
+
   const handleMouseUp = () => {
-    setDrawing(false);
+    setAction("none");
+    setSelectedElement(null);
   };
 
   return (
@@ -89,18 +188,26 @@ function App() {
       <div style={{ position: "fixed", padding: "20px" }}>
         <button
           style={{
-            backgroundColor: `${elementType === "rectangle" ? "red" : ""}`,
+            backgroundColor: `${tool === "selection" ? "red" : ""}`,
+          }}
+          onClick={() => setToolType("selection")}
+        >
+          Selection
+        </button>
+        <button
+          style={{
+            backgroundColor: `${tool === "rectangle" ? "red" : ""}`,
           }}
           onClick={() => {
-            setElementType("rectangle");
+            setToolType("rectangle");
           }}
         >
           Rectangle
         </button>
         <button
-          style={{ backgroundColor: `${elementType === "line" ? "red" : ""}` }}
+          style={{ backgroundColor: `${tool === "line" ? "red" : ""}` }}
           onClick={() => {
-            setElementType("line");
+            setToolType("line");
           }}
         >
           Line
